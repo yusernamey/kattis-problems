bac86e13b5ded89a58f9038bf5741756bd373251
yusernamey <32033973+yusernamey@users.noreply.github.com>
2021-02-10 00:16:37 +0100

    Update hissing microphone.js

--- Hissing Microphone/hissing microphone.js
+++ Hissing Microphone/hissing microphone.js
@@ -2,7 +2,7 @@ const input = readline();
 const inputArray = input.split('');
 let result = "no hiss";
 const doubleLetters = inputArray.reduce((acc, val) => {
-	if ( (val === "s") && (acc === "s") ){
+	if (val === acc){
     	result = "hiss";
     } else {
         acc = val;
